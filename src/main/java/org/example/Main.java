2c61e8cf6c556ec50b3ebfcbf78998eff8306db8
Siddhesh Pisal <siddheshpisal76@gmail.com>
2024-03-24 18:07:21 +0530

    Added okhttp to make http requests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,29 @@
 package org.example;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        for(int i = 1; i <= 10; i++) {
-            System.out.println(i);
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://jsonplaceholder.typicode.com/todos/1";
+
+        Request req = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(req).execute()) {
+            if(!response.isSuccessful()){
+                System.out.println("Something went wrong");
+            }
+            System.out.println("Response Body is : " + response.body().string());
+        }catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
