32bc4ef0d0f259c77fbe38726c945d8d8575b2ed
Siddhesh Pisal <siddheshpisal76@gmail.com>
2024-03-27 20:43:03 +0530

    Added retrofit and made api call using it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,27 +3,41 @@ package org.example;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        OkHttpClient client = new OkHttpClient();
+//        OkHttpClient client = new OkHttpClient();
 
-        String url = "https://jsonplaceholder.typicode.com/todos/1";
+//        String url = "https://jsonplaceholder.typicode.com/todos/1";
+//
+//        Request req = new Request.Builder()
+//                .url(url)
+//                .build();
+//
+//        try (Response response = client.newCall(req).execute()) {
+//            if(!response.isSuccessful()){
+//                System.out.println("Something went wrong");
+//            }
+//            System.out.println("Response Body is : " + response.body().string());
+//        }catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
 
-        Request req = new Request.Builder()
-                .url(url)
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        try (Response response = client.newCall(req).execute()) {
-            if(!response.isSuccessful()){
-                System.out.println("Something went wrong");
-            }
-            System.out.println("Response Body is : " + response.body().string());
-        }catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        TodoService todoService = retrofit.create(TodoService.class);
+
+        Todo t = todoService.getTodoById("1").execute().body();
+
+        System.out.println(t.toString());
     }
 }
